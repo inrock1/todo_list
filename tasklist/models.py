a3d49230f6af81c89e3361001c63a35d997f9eae
Yevhen Kaskov <kaskov.e@gmail.com>
2023-04-03 12:30:09 +0300

    added models, index and tag_list

--- tasklist/models.py
+++ tasklist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(required=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self):
+        return self.content
--- tasklist/views.py
+++ tasklist/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from tasklist.models import Tag
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    # tags = Tag.objects.count()
+    # num_cars = Car.objects.count()
+    # num_manufacturers = Manufacturer.objects.count()
+    #
+    # num_visits = request.session.get("num_visits", 0)
+    # request.session["num_visits"] = num_visits + 1
+    #
+    # context = {
+    #     "num_drivers": num_drivers,
+    #     "num_cars": num_cars,
+    #     "num_manufacturers": num_manufacturers,
+    #     "num_visits": num_visits + 1,
+    # }
+
+    # return render(request, "tasklist/index.html", context=context)
+    return render(request, "tasklist/index.html")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    context_object_name = "tags_list"
+    template_name = "tasklist/tag_list.html"
+    paginate_by = 5
+
+
